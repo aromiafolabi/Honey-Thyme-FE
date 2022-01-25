75baa029bd09a9288405e1d855b8334a34b96320
Aromi Afolabi <aromiafolabi@googlemail.com>
2022-01-25 12:37:27 +0000

    login and register pages

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,6 +1,51 @@
 function Register() {
   return (
-    <h1>Register page</h1>
+    <>
+      <div className="register-header">
+        <h5 className="register-title">
+        Create an Account
+        </h5>
+        <form className="register-text" onSubmit>
+          <label className="authLabel" htmlFor="firstname">
+
+          </label>
+          <input className="regInput"
+            placeholder='First Name'
+            name='first name'
+            onChange
+          />
+          <input
+            placeholder='Last Name'
+            name='last name'
+            onChange
+          />
+          <input
+            placeholder='Email'
+            name='email'
+            onChange
+          />
+          <input
+            placeholder='Username'
+            name='username'
+            onChange
+          />
+          <input
+            placeholder='Password'
+            type="password"
+            name='password'
+            onChange
+          /> 
+          <input
+            placeholder='Confirm password'
+            type="password"
+            name='passwordConfirmation'
+            onChange
+          />
+          <button className="register btn btn-primary" type='submit' id="reg">Register</button>
+        </form>
+        <a href="/login" className="login btn btn-primary" id="log">Login</a>
+      </div>
+    </>
   )
 }
 
